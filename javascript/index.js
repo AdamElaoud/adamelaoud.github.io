8a8ba9a976a00f28c2d25e0034fa071b62dde845
Adam Elaoud <elaoud.adam@gmail.com>
2019-01-19 03:21:30 +0000

    added animated icons in intro and corrected scroll effect to work on all browsers

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
 window.onload = alert("Just a heads up! This website is under construction!");
+
+// scroll Effect
+$(document).ready(function(){
+    // add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // check hash has value
+        if (this.hash !== "") {
+            // prevent default anchor behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // num specifies time to scroll (milliseconds)
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+
+                // restore hash to URL when done scrolling
+                window.location.hash = hash;
+            });
+        }
+    });
+});
